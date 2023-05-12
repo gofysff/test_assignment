a9a0c193167f4665b0e03f036d2240aa55c61987
gofysf <toniush75@gmail.com>
2023-05-12 11:40:01 +0300

    removing redunant

--- tempCodeRunnerFile.dart
+++
@@ -1,6 +0,0 @@
-  // print(
-  //     'api.openweathermap.org/data/2.5/forecast/$apiType?q=$cityName&appid=$apiKey&units=metric');
-  // print(
-  //     'api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid=$apiKey');
-  // print(
-  //     'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=$apiKey');
